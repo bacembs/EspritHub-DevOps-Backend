219c2597f9dcd919c4e90d3630a85c4db20984a3
rayentrabelsi927 <trabelsi.rayen@esprit.tn>
2024-03-31 02:51:28 +0100

    Checkout_Payment

---
+++ src/main/java/tn/esprit/esprithub/services/WebConfig.java
@@ -0,0 +1,19 @@
+package tn.esprit.esprithub.services;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // L'URL de votre API
+                .allowedOrigins("http://localhost:4200") // L'URL de votre application Angular
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
